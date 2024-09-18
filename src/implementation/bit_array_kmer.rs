48e7394276efb9701542c17e95469b37faaf2b3b
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-09-18 11:10:50 +0300

    Implement serde for `BitArrayKmer`.

--- src/implementation/bit_array_kmer.rs
+++ src/implementation/bit_array_kmer.rs
@@ -17,7 +17,7 @@ use std::ops::{Index, Range};
 use traitsequence::interface::{OwnedSequence, Sequence};
 
 /// A k-mer stored as array of minimum-bit characters.
-#[derive(Debug, Clone, Eq, PartialEq, Hash, Ord, PartialOrd)]
+#[derive(Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub struct BitArrayKmer<const K: usize, AlphabetType: Alphabet, BitArrayType = usize>
 where
     BitArrayType: BitViewSized + BitStore,
@@ -137,5 +137,73 @@ impl<
 
     fn index(&self, index: usize) -> &Self::Output {
         self.as_genome_subsequence().index(index)
+    }
+}
+
+impl<const K: usize, AlphabetType: Alphabet, BitArrayType: BitViewSized + BitStore + Clone> Clone
+    for BitArrayKmer<K, AlphabetType, BitArrayType>
+{
+    fn clone(&self) -> Self {
+        Self {
+            phantom_data: PhantomData,
+            array: self.array.clone(),
+        }
+    }
+}
+
+impl<const K: usize, AlphabetType: Alphabet, BitArrayType: BitViewSized + BitStore + Copy> Copy
+    for BitArrayKmer<K, AlphabetType, BitArrayType>
+{
+}
+
+#[cfg(feature = "serde")]
+mod serde {
+    use bitvec::{array::BitArray, store::BitStore, view::BitViewSized};
+    use serde::{Deserialize, Serialize};
+
+    use crate::interface::alphabet::Alphabet;
+
+    use super::BitArrayKmer;
+
+    impl<
+            const K: usize,
+            AlphabetType: Alphabet,
+            BitArrayType: BitViewSized + BitStore + Serialize,
+        > Serialize for BitArrayKmer<K, AlphabetType, BitArrayType>
+    {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            self.array.data.serialize(serializer)
+        }
+    }
+
+    impl<
+            'a,
+            const K: usize,
+            AlphabetType: Alphabet,
+            BitArrayType: BitViewSized + BitStore + Deserialize<'a>,
+        > Deserialize<'a> for BitArrayKmer<K, AlphabetType, BitArrayType>
+    {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'a>,
+        {
+            Ok(Self {
+                phantom_data: Default::default(),
+                array: BitArray {
+                    _ord: Default::default(),
+                    data: BitArrayType::deserialize(deserializer)?,
+                },
+            })
+        }
+
+        fn deserialize_in_place<D>(deserializer: D, place: &mut Self) -> Result<(), D::Error>
+        where
+            D: serde::Deserializer<'a>,
+        {
+            BitArrayType::deserialize_in_place(deserializer, &mut place.array.data)
+        }
     }
 }
