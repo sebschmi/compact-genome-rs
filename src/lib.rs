7f4bb35757fe259bfd7da825eb77225b304fc4e3
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2025-01-20 09:29:35 +0200

    Convert all mod modules to named.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 //! This crate abstracts over the representation of a genome string, allowing for different implementations that are catered to different use-cases.
 #![warn(missing_docs)]
+#![forbid(clippy::mod_module_files)]
 
 /// Different implementations of genome string representations.
 pub mod implementation;
