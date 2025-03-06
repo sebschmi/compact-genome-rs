a5dfcb877da1931997390ff8d9be275626373108
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2025-03-06 09:24:18 +0200

    Add IUPAC amino acid alphabet.

--- src/implementation/alphabets.rs
+++ src/implementation/alphabets.rs
@@ -4,6 +4,7 @@ pub mod dna_alphabet;
 pub mod dna_alphabet_or_n;
 pub mod dna_iupac_nucleic_acid_alphabet;
 pub mod generic_alphabet;
+pub mod iupac_amino_acid_alphabet;
 pub mod rna_alphabet;
 pub mod rna_alphabet_or_n;
 pub mod rna_iupac_nucleic_acid_alphabet;
---
+++ src/implementation/alphabets/iupac_amino_acid_alphabet.rs
@@ -0,0 +1,49 @@
+//! The [IUPAC amino acid alphabet][1].
+//!
+//! [1]: https://web.archive.org/web/20250221074139/https://iupac.qmul.ac.uk/AminoAcid/AA1n2.html
+
+use crate::impl_generic_alphabet;
+
+impl_generic_alphabet!(
+    "IUPAC amino acid alphabet",
+    IupacAminoAcidAlphabet,
+    IupacAminoAcidCharacter,
+    b"ARNDCQEGHILKMFPSTWYVX",
+    b"ARNDCQEGHILKMFPSTWYVX",
+);
+
+#[cfg(test)]
+mod tests {
+    use crate::implementation::alphabets::iupac_amino_acid_alphabet::IupacAminoAcidCharacter;
+    use std::convert::TryFrom;
+
+    #[test]
+    fn test_alphabet_conversion() {
+        let characters = b"ARNDCQEGHILKMFPSTWYVX";
+
+        for ascii in 0u8..=255u8 {
+            if characters.iter().any(|&character| character == ascii) {
+                assert_eq!(
+                    u8::from(
+                        IupacAminoAcidCharacter::try_from(ascii).unwrap_or_else(|_| panic!(
+                            "character {} was expected to be valid, but is not",
+                            ascii
+                        ))
+                    ),
+                    ascii
+                );
+            } else {
+                assert!(IupacAminoAcidCharacter::try_from(ascii).is_err());
+            }
+        }
+    }
+
+    #[test]
+    fn test_display() {
+        for &character in b"ARNDCQEGHILKMFPSTWYVX" {
+            let character = character as char;
+            let dna_character = IupacAminoAcidCharacter::try_from(character).unwrap();
+            assert_eq!(format!("{character}"), format!("{dna_character}"));
+        }
+    }
+}
